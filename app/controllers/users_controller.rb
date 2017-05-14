ac0e4e49709a639b278d9a5807af8f8238adbda2
Danish Saleem <sdanishsaleem@gmail.com>
2017-05-14 10:18:32 +1000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
-  end
-
-
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Free Lancing Website!"
       redirect_to @user
     else
       render 'new'
@@ -24,5 +24,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-
 end
